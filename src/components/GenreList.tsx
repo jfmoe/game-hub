10dc7b2702ec13bbc6c628d341241019254dc7e9
jfmoe <taoxfan@outlook.com>
2024-03-22 10:53:42 +0800

    perf: showing spinner on genre list

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,13 @@
-import { ListItem, List, HStack, Image, Text } from '@chakra-ui/react'
+import { ListItem, List, HStack, Image, Text, Spinner } from '@chakra-ui/react'
 import useGenres from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url'
 
 const GenreList = () => {
-  const { data } = useGenres()
+  const { data, isLoading, error } = useGenres()
+
+  if (isLoading) return <Spinner />
+
+  if (error) return null
 
   return (
     <List>
