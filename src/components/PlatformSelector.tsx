1f18db3be4aaf09c942cf976b69e9b4511e4a54c
jfmoe <taoxfan@outlook.com>
2024-03-24 19:24:54 +0800

    refactor: simplify game querry

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -3,12 +3,14 @@ import { BsChevronDown } from 'react-icons/bs'
 import usePlatforms, { Platform } from '../hooks/usePlatforms'
 
 interface Props {
-  selectedPlatform: Platform | null
+  selectedPlatformId?: number
   onSelectPlatform: (platform: Platform) => void
 }
 
-const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
+const PlatformSelector = ({ selectedPlatformId, onSelectPlatform }: Props) => {
   const { data, error } = usePlatforms()
+
+  const selectedPlatform = data.results.find((platform) => platform.id === selectedPlatformId)
 
   if (error) return null
 
