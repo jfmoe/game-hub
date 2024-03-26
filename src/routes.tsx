1e62066e6400580bba987ee1e16ef2e9b6fc13cb
jfmoe <taoxfan@outlook.com>
2024-03-26 12:59:53 +0800

    build: set router basename

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,22 +4,25 @@ import Layout from './pages/Layout'
 import HomePage from './pages/HomePage'
 import ErrorPage from './pages/ErrorPage'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'games/:slug',
-        element: <GameDetailPage />,
-      },
-    ],
-  },
-])
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: 'games/:slug',
+          element: <GameDetailPage />,
+        },
+      ],
+    },
+  ],
+  { basename: '/game-hub/' }
+)
 
 export default router
